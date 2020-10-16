a90a16afb57a6df20560c2cc595e2d10d6b3adbc
Rafal Krismann <rkrismann33@gmail.com>
2020-10-16 21:51:42 +0100

    7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemDecorator.java
@@ -0,0 +1,5 @@
+package com.kodilla.stream.beautifier;
+
+public interface PoemDecorator {
+    public String beautify(String a, String b);
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/reference/PoemBeautifer.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.reference;
+
+import com.kodilla.stream.beautifier.PoemDecorator;
+
+public class PoemBeautifer {
+
+    public void ExecuteBeautifer(String a,String b,PoemDecorator poemDecorator) {
+        String result = poemDecorator.beautify(a,b);
+        System.out.println("Rezultat: " + result);
+    }
+}
