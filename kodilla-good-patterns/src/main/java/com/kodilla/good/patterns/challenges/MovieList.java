e4941dcb28af82aa2d2ab85fd0d0328050886205
Rafal Krismann <rkrismann33@gmail.com>
2020-12-10 22:29:31 +0000

    13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieList.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieList.java
@@ -2,15 +2,19 @@ package com.kodilla.good.patterns.challenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 class MovieList {
 
     public static void main(String[] args) {
             MovieStore movies = new MovieStore();
-            movies.getMovies().values().stream()
-                    .forEach(name -> System.out.print(" ! " + name));
+            String result = movies.getMovies().values().stream()
+                    .map(Objects::toString)
+                    .collect(Collectors.joining(","));
 
-        }
+            System.out.println(result);
+
+    }
 
 }
